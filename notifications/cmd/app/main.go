c6af1b983c968b17019590c79193572f93630a3b
JeyXX <jeyxxs@gmail.com>
2023-03-31 18:17:12 +0300

    [GOHW-6]: kafka consumer in notifications

--- notifications/cmd/app/main.go
+++ notifications/cmd/app/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"route256/libs/kafka"
+	"route256/notifications/internal/config"
 
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	err := config.Init()
+	if err != nil {
+		log.Fatal("config init", err)
+	}
+
+	brokers := config.ConfigData.Kafka.Brokers
+	c, err := kafka.NewConsumer(brokers)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	orderStateChangeTopicName := config.ConfigData.Kafka.Topics.OrderStateChange.Name
+	orderStateChangeListener := kafka.NewListener(c, orderStateChangeTopicName, func(message *sarama.ConsumerMessage) {
+		log.Println(string(message.Value))
+	})
+	if err = orderStateChangeListener.Subscribe(); err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	<-context.TODO().Done()
 }
---
+++ notifications/internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"os"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+)
+
+type Topic struct {
+	Name string `yaml:"name"`
+}
+
+type Kafka struct {
+	Topics struct {
+		OrderStateChange Topic `yaml:"order_state_change"`
+	} `yaml:"topics"`
+	Brokers []string `yaml:"brokers"`
+}
+
+type ConfigStruct struct {
+	Port  string `yaml:"port"`
+	Kafka Kafka  `yaml:"kafka"`
+}
+
+var ConfigData ConfigStruct
+
+func Init() error {
+	rawYAML, err := os.ReadFile("config.yml")
+	if err != nil {
+		return errors.WithMessage(err, "reading config file")
+	}
+
+	err = yaml.Unmarshal(rawYAML, &ConfigData)
+	if err != nil {
+		return errors.WithMessage(err, "parsing yaml")
+	}
+
+	return nil
+}
